86a2def09f19e8efa5854d241a6ec38dca7d7d79
Fer <luferbrica@gmail.com>
2022-09-17 20:28:00 -0500

    gitignore + ya funciona el ScrollDown

---
+++ web/simpleAnimation.js
@@ -0,0 +1,3 @@
+$(document.getElementById("graficas-continer")).ready(function() {
+    $(document.getElementById("graficas-continer")).scrollTop($(document.getElementById("graficas-continer")).height());
+});
